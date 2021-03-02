c5c51d7a25d61cfff0fd1331a0fc99f198961440
vamsikrishna-api <75066513+vamsikrishna-api@users.noreply.github.com>
2021-03-02 21:37:45 +0530

    feat:commit fix

--- src/main/java/com/beis/subsidy/ga/schemes/dbpublishingservice/service/impl/SubsidySchemeServiceImpl.java
+++ src/main/java/com/beis/subsidy/ga/schemes/dbpublishingservice/service/impl/SubsidySchemeServiceImpl.java
@@ -241,7 +241,7 @@ public class SubsidySchemeServiceImpl implements SubsidySchemeService {
         List<SubsidyMeasure> subsidyMeasuresList = subsidyMeasureRepository.findBySubsidyMeasureTitle(scName);
        Optional<SubsidyMeasure> dbsubsidyMeasure=subsidyMeasuresList.stream().filter(subsidyMeasure->"Active".equalsIgnoreCase(subsidyMeasure.getStatus()) && searchScheme.getGaId()==subsidyMeasure.getGaId()).findAny();
        
-       log.warn("inside findSubsidySchemeByName "+dbsubsidyMeasure);
+       log.warn("inside findSubsidySchemeByName value "+dbsubsidyMeasure);
         if(dbsubsidyMeasure.isPresent()) {
         log.info(" subsidyMeasure from db,status "+dbsubsidyMeasure.get().getSubsidyMeasureTitle()+" : "+dbsubsidyMeasure.get().getStatus()+" : "+dbsubsidyMeasure.get().getGaId());
         return true;
